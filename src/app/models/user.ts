e970ec92ee7eef6b10a4d48a620bb0582e117e0f
CobyBoy <aboutacoby@gmail.com>
2023-01-28 13:44:27 -0300

    Change to dto and added phone number

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,8 @@
-export interface User {
+export interface UserDto {
   username: string;
   email: string;
   password: string;
   token?: string;
   confirmPassword?: string;
+  phoneNumber: string;
 }
