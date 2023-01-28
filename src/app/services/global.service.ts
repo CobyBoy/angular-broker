4fdc01123676aa84758815fd27daff574ae4b509
CobyBoy <aboutacoby@gmail.com>
2023-01-28 01:15:49 -0300

    Created spinner

---
+++ src/app/services/global.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalService {
+  public $isLoading = new BehaviorSubject<boolean>(false);
+  constructor() { }
+}
