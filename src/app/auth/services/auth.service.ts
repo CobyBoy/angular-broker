f65e54db1483f600b84d8c217e6d1f8d60ba3a11
CobyBoy <aboutacoby@gmail.com>
2023-01-28 13:45:04 -0300

    Change to dto and added response interface

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, shareReplay, tap } from 'rxjs';
+import { Observable, shareReplay, tap } from 'rxjs';
 import { Credentials } from 'src/app/models/credentials';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { AuthenticationResponse } from 'src/app/models/authentication-response';
-import { User } from 'src/app/models/user';
+import { UserDto } from 'src/app/models/user';
+import { SignUpResponse } from 'src/app/models/sign-up-response';
 
 @Injectable({
   providedIn: 'root',
@@ -14,11 +15,9 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  signUp(user: User): Observable<any> {
-    console.warn(user)
+  signUp(user: UserDto): Observable<SignUpResponse> {
     const url = `${environment.host}/auth/register`;
-    return this.http.post<any>(url, user)
-    return of({})
+    return this.http.post<SignUpResponse>(url, user);
   }
 
   logIn(credentials: Credentials): Observable<any> {
