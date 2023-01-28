9bd9fc2718bcd7c5742f2b678561aef4d4c11f73
CobyBoy <aboutacoby@gmail.com>
2023-01-28 01:18:22 -0300

    Added components to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from '../components/spinner/spinner.component';
+import { ToastComponent } from '../components/toast/toast.component';
 
 
 
 @NgModule({
   declarations: [
-    SpinnerComponent
+    SpinnerComponent,
+    ToastComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [SpinnerComponent, ToastComponent]
+  
 })
 export class SharedModule { }
