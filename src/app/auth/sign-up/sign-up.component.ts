100af0d779c97a7ac6809b2edcc62b94f9d8410c
CobyBoy <aboutacoby@gmail.com>
2023-01-28 17:26:10 -0300

    Added regex validation, error msg and desubscription

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,23 +1,25 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { ToastService } from '../../services/toast.service';
 import { GlobalService } from 'src/app/services/global.service';
 import { UserDto } from 'src/app/models/user';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
   @ViewChild('phoneNumber') phoneNumber!: ElementRef;
   loginForm!: FormGroup;
   isEyeIconOpen: boolean = false;
   isEyeIconOpenForConfirmPassword: boolean = false;
   fieldRequiredText = 'This field is required';
   arePasswordsEqual: boolean = false;
+  private destroy$ = new Subject();
 
   constructor(
     private authService: AuthService,
@@ -31,41 +33,46 @@ export class SignUpComponent implements OnInit {
     this.arePasswordsEqual =
       this.loginForm.controls['password'].value ===
       this.loginForm.controls['confirmPassword'].value;
-    this.loginForm.valueChanges.subscribe((control) => {
-      this.arePasswordsEqual = control.password === control.confirmPassword;
-      if (control.areaCode.length == 4) {
-        this.phoneNumber.nativeElement.focus();
-      }
-    });
+    this.loginForm.valueChanges
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$))
+      .subscribe((control) => {
+        this.arePasswordsEqual = control.password === control.confirmPassword;
+        if (control.areaCode.length == 4) {
+          this.phoneNumber.nativeElement.focus();
+        }
+      });
     console.log(this.loginForm);
   }
 
   createForm(): FormGroup {
     return new FormGroup({
-      username: new FormControl('coby', [
+      username: new FormControl('coby5', [
         Validators.required,
         Validators.minLength(4),
-        Validators.maxLength(10),
+        Validators.maxLength(64),
       ]),
       email: new FormControl('abouta@gmail.com', [
+        Validators.required,
         Validators.email,
+        Validators.minLength(3),
+        Validators.maxLength(254),
+      ]),
+      password: new FormControl('@Cc1234', [
+        Validators.minLength(4),
         Validators.required,
       ]),
-      password: new FormControl('1111', [
-        Validators.minLength(4),
-        Validators.maxLength(16),
-        Validators.required,
-      ]),
-      confirmPassword: new FormControl('1111', [
+      confirmPassword: new FormControl('@Cc1234', [
         Validators.required,
         Validators.minLength(4),
-        Validators.maxLength(16),
       ]),
       areaCode: new FormControl('011', [
         Validators.required,
         Validators.maxLength(4),
       ]),
-      phoneNumber: new FormControl('64479914', [
+      phoneNumber: new FormControl('124569', [
         Validators.required,
         Validators.maxLength(8),
       ]),
@@ -77,17 +84,13 @@ export class SignUpComponent implements OnInit {
   }
 
   onSignUp(_event: SubmitEvent) {
-    this.globalService.$isLoading.next(true);
     Object.values(this.loginForm.controls).forEach((control) => {
       control.markAllAsTouched();
     });
     console.log(this.loginForm.invalid);
     if (this.loginForm.invalid) return;
-    // put together areacode + phoneNumber
-    Object.assign(this.loginForm.value, { ...this.loginForm.value, phoneNumber: this.loginForm.value.areaCode + this.loginForm.value.phoneNumber });
-    // get rid of areaCode
-    const { areaCode, ...values } = this.loginForm.value
-    this.authService.signUp(values as UserDto).subscribe({
+    this.globalService.$isLoading.next(true);
+    this.authService.signUp(this.loginForm.value as UserDto) .subscribe({
       next: (response) => {
         this.globalService.$isLoading.next(false);
         this.toastService.success(response.message);
@@ -106,5 +109,10 @@ export class SignUpComponent implements OnInit {
   viewConfirmPassword() {
     this.isEyeIconOpenForConfirmPassword =
       !this.isEyeIconOpenForConfirmPassword;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(undefined);
+    this.destroy$.complete();
   }
 }
